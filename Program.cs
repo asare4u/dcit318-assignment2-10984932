f96c9974a2c8fa6252c9607610c47b06d3a21d69
asare4u <nkansahasare3@gmail.com>
2024-06-16 23:34:42 +0000

    Add modified Program.cs file

--- Program.cs
+++ Program.cs
@@ -1,34 +1,12 @@
-﻿//Implementing Task 1 - Inheritance and Method Overriding
-//Defining the base class 'Animal'
-public class Animal {
-  public virtual void MakeSound() {
-    Console.WriteLine("Some generic sound");
-  }
-}
+﻿using System;
 
-//Defining the derived class 'Dog'
-public class Dog : Animal {
-  public override void MakeSound() {
-    Console.WriteLine("Bark");
-  }
-}
-
-//Defining the derived class 'Cat'
-public class Cat : Animal {
-  public override void MakeSound() {
-    Console.WriteLine("Meow");
-  }
-}
-
-//Implementing the 'Main' method
-class Program {
-  static void Main(string[] args) {
-    Animal animal = new Animal();
-    Dog dog = new Dog();
-    Cat cat = new Cat();
-
-    animal.MakeSound(); // Output: Some generic sound
-    dog.MakeSound(); // Output: Bark
-    cat.MakeSound(); // Output: Meow
-  }
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Calling methods for each task
+        Task1.Execute();
+        Console.WriteLine();
+        Task2.Execute();
+    }
 }
